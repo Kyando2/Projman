3aa61f4a8c0c74616b80a2ecb958f43f26a7ad2c
Kyando2 <amehikoji@gmail.com>
2020-09-24 10:36:37 -0400

    Added c_switch

--- src/exts/c_switch.py
+++ src/exts/c_switch.py
@@ -1,2 +1,18 @@
+import os
+import sys
+
+import Kyandle
+
 def handler(args, gb):
-    pass
+    if os.path.exists(gb + "\\config\\environment.kya"):
+        with open(gb + "\\config\\environment.kya", "r") as f:
+            data = Kyandle.parse(f.read())
+        data["current"] = args.name
+    else:
+        print("Please run projman init before trying to switch env")
+        sys.exit()
+
+    with open(gb + "\\config\\environment.kya", "w") as f:
+        f.write(Kyandle.serialize(data))
+    
+    print("Successfully changed environment")
